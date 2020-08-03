9c476c2063163915dfb91febc6eea60a6d986758
wdyjason <ddd123321456555@qq.com>
2020-08-03 21:50:53 +0800

    finish refactoring exercise 4

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,6 +1,10 @@
 package com.twu.refactoring;
 
 public class NumberCruncher {
+    private final  int CONDITION_ODD = 0;
+    private final  int CONDITION_EVEN = 1;
+    private final  int CONDITION_POSITIVE = 2;
+    private final  int CONDITION_NEGATIVE = 3;
     private final int[] numbers;
 
     public NumberCruncher(int... numbers) {
@@ -8,33 +12,28 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return countInCondition(CONDITION_EVEN);
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return countInCondition(CONDITION_ODD);
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return countInCondition(CONDITION_POSITIVE);
     }
 
     public int countNegative() {
+        return countInCondition(CONDITION_NEGATIVE);
+    }
+
+    public int countInCondition(int conditon) {
         int count = 0;
         for (int number : numbers) {
-            if (number < 0) count++;
+            if (conditon == CONDITION_EVEN && number % 2 == 0) count++;
+            if (conditon == CONDITION_ODD && number % 2 == 1) count++;
+            if (conditon == CONDITION_POSITIVE && number >= 0) count++;
+            if (conditon == CONDITION_NEGATIVE && number < 0) count++;
         }
         return count;
     }
